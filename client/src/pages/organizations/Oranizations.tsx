c63f501c55d7d1b3abad2e19c3451c9f85827d0a
Clanie1 <danybarociov@gmail.com>
2023-06-08 04:42:52 -0700

    mock organizations db

--- client/src/pages/organizations/Oranizations.tsx
+++ client/src/pages/organizations/Oranizations.tsx
@@ -12,39 +12,66 @@ import {
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-function OrganizationCard() {
+const organizations = [
+  {
+    title: "4 PATAS",
+    wallet: "5FA57NG5AcDDyRuXCWLcSXMAPrWZQZmfBtQxVeUNbhawWGHw",
+    img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/849/themes/common/logo-1866482157-1574635760-6cf4fcbbca71b55a01145347adaf5bde1574635760.png?0",
+  },
+
+  {
+    title: "Doctores Sin Fronteras",
+    wallet: "5E7GBcbwWsYjhufaUrooxmDGXeMnpidQ61tYus9hzv8B634i",
+    img: "https://www.sustainable.pitt.edu/wp-content/uploads/2022/02/doctors@2x.png",
+  },
+  {
+    title: "ONU",
+    wallet: "5HMYXHkRxtrWchbosFJLMpARWhdLPz7z7kGCAshdjKweAkyF",
+    img: "https://www.un.org/sites/un2.un.org/files/2021/03/un-logo.png",
+  },
+  {
+    title: "World Wildlife Fund",
+    wallet: "5G7PvWJ3XPbPEAXAVSUiKSX6PrvssEhsdi8vBoeGd7aMBCvs",
+    img: "https://1000marcas.net/wp-content/uploads/2020/02/logo-WWF.png",
+  },
+];
+
+function OrganizationCard({ title, wallet, img }: any) {
   return (
     <Card padding="10px" width="full">
       <Flex>
         <Image
           boxSize="60px"
           borderRadius="full"
-          src="https://bit.ly/dan-abramov"
+          src={img}
           alt="algo"
           marginRight="15px"
+          objectFit="cover"
         />
         <Center>
           <Stack>
-            <Text fontSize="xl" color="#">
-              CETYS universidad
+            <Text fontSize="xl" color="#" fontWeight="semibold">
+              {title}
             </Text>
           </Stack>
         </Center>
         <Spacer />
-        <Center>
-          <Text fontSize="lg" fontWeight="light">
-            <span style={{ fontWeight: "bold", marginRight: "10px" }}>
-              11231
-            </span>
-            total donations
-          </Text>
-        </Center>
-        <Spacer />
-        <Center>
-          <Button colorScheme="facebook" size="sm">
-            <Link to="/organizations">View Organizacion</Link>
-          </Button>
-        </Center>
+        <Flex gap="3em">
+          <Center>
+            <Text fontSize="md" fontWeight="light">
+              <span style={{ fontWeight: "bold", marginRight: "10px" }}>
+                11231
+              </span>
+              total donations
+            </Text>
+          </Center>
+          <Spacer />
+          <Center>
+            <Button colorScheme="facebook" size="sm">
+              <Link to="/organizations">View Organizacion</Link>
+            </Button>
+          </Center>
+        </Flex>
       </Flex>
     </Card>
   );
@@ -54,8 +81,9 @@ function Organizations() {
   return (
     <Container maxW="container.lg" marginTop="10px">
       <VStack spacing="1em">
-        <OrganizationCard />
-        <OrganizationCard />
+        {organizations.map((organization) => (
+          <OrganizationCard title={organization.title} img={organization.img} />
+        ))}
       </VStack>
     </Container>
   );
