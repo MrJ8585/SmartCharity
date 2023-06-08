d8192cb638f4fea670be1286940b8a1c60735d98
Adrián Fernández <oscar.adrian.fersa@gmail.com>
2023-06-07 19:38:36 -0700

    Added a new page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
-import { Home } from './home';
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./home";
+import { Ongs } from "./ongs";
 
-const routes = [{ path: '/', Page: Home }];
+const routes = [
+  { path: "/", Page: Home },
+  { path: "/ongs", Page: Ongs },
+];
 
 function Routing() {
-  const getRoutes = () => routes.map(({ path, Page }) => <Route key={path} path={path} element={<Page />} />);
+  const getRoutes = () =>
+    routes.map(({ path, Page }) => (
+      <Route key={path} path={path} element={<Page />} />
+    ));
 
   return <Routes>{getRoutes()}</Routes>;
 }
