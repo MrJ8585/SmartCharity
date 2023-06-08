29dda5e77acf7cb817cb93d32cb06083b1164f5e
Oscar Montoya <98506040+oscark22@users.noreply.github.com>
2023-06-07 18:58:30 -0700

    almost nothing

--- web3/examples/starter/src/lib.rs
+++ web3/examples/starter/src/lib.rs
@@ -1,11 +1,13 @@
 #![no_std]
+use gstd::{prelude::*, msg, debug};
 
 #[no_mangle]
-extern "C" fn init(){
-
+extern "C" fn init() {
+    let init_message: String = msg::load().expect("Can't load the incoming message");
+    debug!("Program initialized {:?}", init_message);
 }
 
 #[no_mangle]
-extern "C" fn handle(){
-    
+extern "C" fn handle() {
+    msg::reply(String::from("Hello"), 0).expect("Error in sending a reply message");
 }
